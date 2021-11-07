18666cd034d45163a65f8ce5851d727d892d0f73
pedro <pedromsantoslopes04@gmail.com>
2021-11-07 17:42:16 +0000

    Fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const withTM = require('next-transpile-modules')(['somemodule', 'and-another']); // pass the modules you would like to see transpiled
+const withTM = require('next-transpile-modules')(['next-transpile-modules']); // pass the modules you would like to see transpiled
 
 module.exports = withTM({
   reactStrictMode: true,
